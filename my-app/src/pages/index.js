d17d885438829ca8e6aad4437fecb25fe3f2f4ca
Gerald-Z <geraldjialeizhu@gmail.com>
2023-08-17 20:21:07 -0500

    Converted index and main pages from HTML to JSX

---
+++ my-app/src/pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Index = () => {
+    return (
+        <form>
+            <fieldset>
+                <div className="center"><p>Log in to Stock Portfolio Tracker</p></div>
+                <div className="center"><label id="username-label">Username: <input type="text" className="textInput" required /> </label></div>
+                <div className="center"><label id="password-label">Password: <input type="password" className="textInput" required /> </label></div>
+                <div className="center"><input type="submit" className="textInput" id="submit" value="Log In" />  </div>
+                <div className="space">
+                    <div className="center"><a href="new_user"><p>New user? Click here to create an account!</p></a></div>
+                    <div className="center"><a href="main"><p>Click here to get to a template portfolio</p></a></div>
+                </div>
+            </fieldset>
+        </form>
+    )
+}
+
+
+
+
+export default Index;
---
+++ my-app/src/pages/main.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const callAPI = function() {
+    var apiUrl = 'http://localhost:4400/api/portfolio';
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(response => {
+            document.getElementById('changed').value = response.data;
+            return;
+        })
+        .catch(err => {
+        // Do something for an error here
+        });
+}
+
+
+
+const Main = () => {
+    return (
+        <form>
+            <fieldset>
+                <div className="center"><p>Here Are Your Current Positions:</p></div>
+                <table className="stock-list">
+                    <tr>
+                        <th>Company Name</th>
+                        <th>Ticker Symbol</th>
+                        <th>Shares Owned</th>
+                        <th>Share Price</th>
+                        <th>Total Cost</th>
+                        <th>Total Value</th>
+                        <th>Estimated Dividend Payout</th>
+                        <th>Estimated Dividend Yield</th>
+                    </tr>
+                    <tr>
+                        <td>JP Morgan</td>
+                        <td>JPM</td>
+                        <td>100</td>
+                        <td>100</td>
+                        <td>10000</td>
+                        <td>10000</td>
+                        <td>100</td>
+                        <td>100</td>
+                    </tr>
+                    <tr>
+                        <td>JP Morgan</td>
+                        <td>JPM</td>
+                        <td>100</td>
+                        <td>100</td>
+                        <td>10000</td>
+                        <td>10000</td>
+                        <td>100</td>
+                        <td>100</td>
+                    </tr>
+                </table>
+                
+                <div className="space">
+                    <div className="center"><a href="new_user.html"><p>Click Here to Add a Position</p></a></div>
+                </div>
+
+            <input id="changed" value="text" />
+            </fieldset>
+            <button onClick='callAPI()'>Click here to call API</button>
+
+        </form>
+    )
+}
+
+
+
+
+export default Main;
