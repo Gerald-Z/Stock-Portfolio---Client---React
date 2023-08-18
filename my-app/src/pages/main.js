06ae3d7c307320bb67eed260e1198771c7e9eb70
Gerald-Z <geraldjialeizhu@gmail.com>
2023-08-18 10:49:53 -0500

    Added API calls that correctly populate tables

--- my-app/src/pages/main.js
+++ my-app/src/pages/main.js
@@ -2,70 +2,60 @@ import React from 'react';
 import './main.css'
 
 
-const Main = () => {
+class Main extends React.Component {
     
+    constructor(props) {
+        super(props);
+        this.state = {
+            stock : {}
+        }
+    }
 
 
-    const callAPI = () => {
-        var apiUrl = 'http://localhost:4400/api/portfolio';
+    callAPI() {
+        const apiUrl = 'http://localhost:4400/api/portfolio';
     
         fetch(apiUrl)
             .then(response => response.json())
             .then(response => {
-                document.getElementById('changed').value = response.data;
-                return;
+            //    document.getElementById('changed').value = response.data;
+                console.log(response.data, "is retunred");
+                this.setState({"stock": response.data});
             })
             .catch(err => {
             // Do something for an error here
             });
     };
 
-    return (
+    componentDidMount() {
+        this.callAPI();
+    //    document.getElementById('changed').value = (this.state.stock);
+      //  console.log(this.state.stock);
+    }
+
+    render() {return (
         <form>
             <fieldset>
                 <div className="center"><p>Here Are Your Current Positions:</p></div>
                 <table className="stock-list">
                     <tr>
-                        <th>Company Name</th>
-                        <th>Ticker Symbol</th>
-                        <th>Shares Owned</th>
-                        <th>Share Price</th>
-                        <th>Total Cost</th>
-                        <th>Total Value</th>
-                        <th>Estimated Dividend Payout</th>
-                        <th>Estimated Dividend Yield</th>
-                    </tr>
-                    <tr>
-                        <td>JP Morgan</td>
-                        <td>JPM</td>
-                        <td>100</td>
-                        <td>100</td>
-                        <td>10000</td>
-                        <td>10000</td>
-                        <td>100</td>
-                        <td>100</td>
-                    </tr>
-                    <tr>
-                        <td>JP Morgan</td>
-                        <td>JPM</td>
-                        <td>100</td>
-                        <td>100</td>
-                        <td>10000</td>
-                        <td>10000</td>
-                        <td>100</td>
-                        <td>100</td>
+                        <th>{this.state.stock.CompanyName}</th>
+                        <th>{this.state.stock.Ticker}</th>
+                        <th>{this.state.stock.SharesOwned}</th>
+                        <th>{this.state.stock.SharePrice}</th>
+                        <th>{this.state.stock.TotalCost}</th>
+                        <th>{this.state.stock.TotalValue}</th>
+                        <th>{this.state.stock.EstimatedDivPayout}</th>
+                        <th>{this.state.stock.EstimatedDivYield}</th>
                     </tr>
                 </table>
                 
                 <div className="space">
                     <div className="center"><a href="new_user.html"><p>Click Here to Add a Position</p></a></div>
                 </div>
-
-            <input id="changed" value="text" />
             </fieldset>
-            <button onClick={callAPI}>Click here to call API</button>
         </form>
-    )
+    )}
 }
 
 
