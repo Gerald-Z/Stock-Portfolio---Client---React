3753e6ce3d7bff2df877e97de11294deab45539c
Gerald-Z <geraldjialeizhu@gmail.com>
2023-08-19 12:21:34 -0500

    Used HashRouter to make GitHub Pages work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,26 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import Index from './pages/index.js'
 import Main from './pages/main.js'
 import PositionChanges from './pages/newPosition.js'
 
-const baseUrl = 'https://gerald-z.github.io/Stock-Portfolio---Client---React/';
-
-       // <Route path={baseUrl} element={<Index />} />
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
-        <Route path='https://gerald-z.github.io/Stock-Portfolio---Client---React/' element={<Index />} />
+        <Route path='/' element={<Index />} />
 
-        <Route path={baseUrl+'main'} element={<Main />} />
-        <Route path={baseUrl+'position_changes'} element={<PositionChanges />} />
+        <Route path='main' element={<Main />} />
+        <Route path='position_changes' element={<PositionChanges />} />
 
       </Routes>
-    </Router>
+    </HashRouter>
+
+
   );
 }
 
