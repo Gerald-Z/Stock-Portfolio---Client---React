a8578df4c42b7a347bf5bbd69b7a292ad4e03bac
Gerald-Z <geraldjialeizhu@gmail.com>
2023-08-19 13:58:53 -0500

    Created a basic cypress test

--- my-app/cypress/e2e/spec.cy.js
+++ my-app/cypress/e2e/spec.cy.js
@@ -1,5 +1,19 @@
 describe('Login Page Works', () => {
   it('Visits the login page', () => {
     cy.visit('https://gerald-z.github.io/Stock-Portfolio---Client---React/')
-  })
+
+    cy.contains('template').click()
+    
+  });
+
+  it('Edits the Portfolio', () => {
+    cy.visit('https://gerald-z.github.io/Stock-Portfolio---Client---React/')
+    cy.contains('template').click()
+
+    cy.contains('Bought').click()
+    cy.get('label > input').type("GS")
+    cy.get('#submitTicker').click()
+  
+  });
+
 })
